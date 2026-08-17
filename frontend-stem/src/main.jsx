bc879421f5f2577e0b61c3ba6545cbed89c33bf3
Qydud <104167445+Qydud@users.noreply.github.com>
2026-08-17 19:04:42 +0500

    refactor: remove STEM frontend providers and use ServiceHub shell

--- frontend-stem/src/main.jsx
+++ frontend-stem/src/main.jsx
@@ -1,37 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
 
 import './index.css'
-
 import App from './App.jsx'
-
-import { LanguageProvider } from './i18n/LanguageContext'
-import { CartProvider } from './context/CartContext'
-import { FavoritesProvider } from './context/FavoritesContext'
-import { UserEmailProvider } from './context/UserEmailContext'
-import { AuthProvider } from './context/AuthContext'
-import { LocationProvider } from './context/LocationProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <LanguageProvider>
-          <LocationProvider>
-            <UserEmailProvider>
-              <AuthProvider>
-                <CartProvider>
-                  <FavoritesProvider>
-                    <App />
-                  </FavoritesProvider>
-                </CartProvider>
-              </AuthProvider>
-            </UserEmailProvider>
-          </LocationProvider>
-        </LanguageProvider>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
