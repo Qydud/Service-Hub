3bb2edf8757fcc14c9176079ca524d9ee474a12c
Qydud <104167445+Qydud@users.noreply.github.com>
2026-08-17 20:11:58 +0500

    feat(admin): route /admin to dedicated dashboard

--- frontend-stem/src/main.jsx
+++ frontend-stem/src/main.jsx
@@ -4,11 +4,17 @@ import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
 import App from './App.jsx'
+import AdminApp from './AdminApp.jsx'
+
+function Root() {
+  const isAdminRoute = window.location.pathname === '/admin' || window.location.pathname.startsWith('/admin/')
+  return isAdminRoute ? <AdminApp /> : <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Root />
     </BrowserRouter>
   </React.StrictMode>,
 )
